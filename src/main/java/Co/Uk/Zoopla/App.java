3e1b029def44c3bffc0bca6160b2e233cc47e567
Kellymicheal <olagunju08@yahoo.com>
2020-03-17 05:20:23 +0000

    adding initial test framework

---
+++ src/main/java/Co/Uk/Zoopla/App.java
@@ -0,0 +1,13 @@
+package Co.Uk.Zoopla;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
